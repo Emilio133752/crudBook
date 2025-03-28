7dde1327e6456f672d3947f1d59580fc158b2a81
Emilio <emiliocantil726@gmail.com>
2025-03-28 20:50:02 -0300

    corrigindo bug do builder.service e criando metodo get e getid

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -1,7 +1,51 @@
-﻿namespace crudBook.Controllers
-{
-    public class BooksController
-    {
+﻿using System.Linq.Expressions;
+using crudBook.Data;
+using crudBook.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
+namespace crudBook.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public BooksController(AppDbContext context)
+        {
+            _context = context;
+        }
+        //Metodo Get
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Book>>> GetBooks() 
+        {
+            try 
+            {
+                var books = await _context.Books.ToListAsync();
+                return Ok(books);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }  
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> Getbook(int id) 
+        {
+
+            try 
+            {
+                var book = await _context.Books.FindAsync(id);
+                if (book == null) return NotFound();
+                return book;
+            }
+             catch(Exception ex)
+             {
+                return StatusCode(500, ex.Message);
+            }
+
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,11 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options =>
+   options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-   options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
