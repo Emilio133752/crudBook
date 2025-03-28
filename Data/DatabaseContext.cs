cda2ec180bec792d9039b7e495055691da280da5
Emilio <emiliocantil726@gmail.com>
2025-03-28 18:15:13 -0300

    API backend csharp, instalação e configuração do projeto, db context para conexão sql server

---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace crudBook.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options ) : base(options)
+        {
+
+        }
+    }
+}
